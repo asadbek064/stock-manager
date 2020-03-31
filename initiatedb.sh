61bf2539ee35b36f1ad958683b70e674f66afd74
user1 <hi@almb.me>
2020-03-31 15:21:00 -0400

    created a bash script to initiable the tables

--- initiatedb.sh
+++ initiatedb.sh
@@ -6,20 +6,29 @@
 
 mysql -u $1 -p$2 <<SETUP_SCHEMA
 use stock_manager;
-CREATE TABLE IF NOT EXISTS users (ID int(11) AUTO_INCREMENT,
-username varchar(255) NOT NULL,
-firstName varchar(255) NOT NULL,
-lastName varchar(255) NOT NULL,
-email varchar(255) NOT NULL,
-pwd varchar(255) NOT NULL,
-PRIMARY KEY  (ID));
-CREATE TABLE IF NOT EXISTS user_stock_alert (ID int(11) AUTO_INCREMENT,
-stockName varchar(50) NOT NULL,
-targetPrice INT(50) NOT NULL,
-PRIMARY KEY  (ID));
+CREATE TABLE users(
+username_ID INT AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+firstName VARCHAR(255) NOT NULL,
+lastName VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+pwd VARCHAR(255) NOT NULL
+); 
+CREATE TABLE user_stock_alert(
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+username_id INT,
+stockName VARCHAR(255) NOT NULL,
+targetPrice DOUBLE,
+ifGTRtarget BOOLEAN,
+ifLStarget BOOLEAN,
+isCompleted BOOLEAN,
+FOREIGN KEY(username_id) REFERENCES users(username_ID));
 CREATE TABLE IF NOT EXISTS stocks (ID int(11) AUTO_INCREMENT,
 stockName varchar(50) NOT NULL,
 StockSymbol varchar(10) NOT NULL,
 stockPrice INT(10) NOT NULL,
 PRIMARY KEY  (ID));
+CREATE TABLE IF NOT EXISTS stock_list (
+description VARCHAR(255) NOT NULL,
+symbol VARCHAR(255) NOT NULL);
 SETUP_SCHEMA
